a5eaedaf6cb9eaf51c03b0a7727943b0bef4f396
Kevin Urban <urban.kevi@uwlax.edu>
2017-07-16 12:54:55 -0500

    Added basic scanner user input functionality to the java program so that the users can input numbers

--- NumeralWriter.java
+++ NumeralWriter.java
@@ -2,8 +2,15 @@
   Date: 7/16/2017
 */
 
+//Import statements
+import java.util.Scanner;
+
 public class NumeralWriter {
 	public static void main(String[] args){
 		System.out.println("Start of File");
+		System.out.println("Please enter a number");
+		Scanner scan = new Scanner(System.in);
+		String number = scan.nextLine();
+		System.out.println("\nThe Number given is: " + number);
 	}
 }
